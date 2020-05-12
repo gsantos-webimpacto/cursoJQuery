d4989b613e7739648248c6b7c9fae51ce4521d4f
guilermosantos <gsantos@webimpacto.es>
2020-05-12 13:25:35 +0200

    43. Eliminar mediante ajax y SweetAlert

--- ajax2/js/select.js
+++ ajax2/js/select.js
@@ -19,14 +19,14 @@
             }
             data.alumnos.forEach(function(alumno,index){
                 var content="";
-                content+='   <tr>';
+                content+='   <tr id="fila'+alumno.id+'">';
                 content+='   <td>'+alumno.id+'</td>';
                 content+='   <td>'+alumno.nombre+'</td>';
                 content+='   <td>';
                 content+='   <a href="actualizar.html?id='+alumno.id+'" class="btn btn-primary"><i class="fa fa-edit"></i></a>';
                 content+='   </td>';
                 content+='   <td>';
-                content+='       <a href="" data-id="'+alumno.id+'" class="btn btn-danger"><i class="fa fa-trash-o"></i></a>';
+                content+='       <a href="" data-nombre="'+alumno.nombre+'" data-id="'+alumno.id+'" class="btn btn-danger btnEliminar"><i class="fa fa-trash-o"></i></a>';
                 content+='   </td>';
                 content+='   </tr>';
                 $("#tblRegistros").append(content);
@@ -38,7 +38,45 @@
         
     });
 
+    $("body").on("click",".btnEliminar",function( e ){
+        e.preventDefault();
+        var nombre = $(this).data('nombre');
+        var id= $(this).data('id');
+        swal({
+            title: "¿Estás seguro?",
+            text: "De querer borrar a:"+nombre,
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+                borrarRegistro(id);
+            } else {
+              swal("Cancelado");
+            }
+          });
 
+        //Cuando estamos seguros que lo deseamos borrar
+       
+    
+    });
 
+    function borrarRegistro(id){
+        // var id= $(this).data('id');
+        // console.log(id);
+
+        $.ajax({
+			type: 'POST',
+			url : 'php/servicios/post.eliminaralumno.php?id='+id,
+			dataType: 'json',
+        })
+        .done(function( data ){
+            console.log("Correcto!");
+            console.log( data ); // Se imprime en consola la api
+            $("#fila"+id).remove();
+            swal("Borrado! El regiistro ha sido eliminado correctamente!", {icon: "success"});
+        });
+    }
 
 })();
